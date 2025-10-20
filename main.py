cfc800763709f080ca43f2564fab8de0b7a59c64
Waleed-Ansar <waleedansar570@outlook.com>
2025-10-20 06:45:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from fastapi import APIRouter, FastAPI
-from api import API
-import uvicorn
+# from fastapi import APIRouter, FastAPI
+# from api import API
+# import uvicorn
 
-app = FastAPI()
-router = APIRouter()
-api = API()
+# app = FastAPI()
+# router = APIRouter()
+# api = API()
 
-router.include_router(api.router)
+# router.include_router(api.router)
 
-app.include_router(router, prefix='/book_ai')
+# app.include_router(router, prefix='/book_ai')
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=1000,
-        reload=True
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=1000,
+#         reload=True
 
-    )
+#     )
+
