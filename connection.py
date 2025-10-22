0b7a9a388cdd324f76e1e2043db8804063b5e1d6
Waleed-Ansar <waleedansar570@outlook.com>
2025-10-21 23:47:03 -0700

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,7 +1,19 @@
+import ssl
 from motor.motor_asyncio import AsyncIOMotorClient
 
-client = AsyncIOMotorClient("mongodb+srv://Waleed-Ansar:fastAPI%40waleed570@fastapi-cluster.0yfuiu4.mongodb.net/", tls=True, tlsAllowInvalidCertificates=False)
-db = client["Book_Shelf"]
-collection_name = db['Books']
-api_keys = db['api_key']
+MONGO_URI = "mongodb+srv://Waleed-Ansar:fastAPI%40waleed570@fastapi-cluster.0yfuiu4.mongodb.net/"
 
+ssl_ctx = ssl.create_default_context()
+ssl_ctx.check_hostname = True
+ssl_ctx.verify_mode = ssl.CERT_REQUIRED
+
+client = AsyncIOMotorClient(
+    MONGO_URI,
+    tls=True,
+    tlsAllowInvalidCertificates=False,
+    tlsCAFile=None
+)
+
+db = client["Book_Shelf"]
+collection_name = db["Books"]
+api_keys = db["api_key"]
